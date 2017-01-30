3fe59332a80f9f7e729e86cb823a3b04d32505fc
Marc Llopis <marcllopis89@gmail.com>
2017-01-30 09:56:11 +0100

    Random shuffle the stack and separate it in 4 stacks

--- js/main.js
+++ js/main.js
@@ -32,6 +32,7 @@ When turns are passung, the stack of players will be decreasing, so:
 
 //Create the main object
 function JungleSpeed() {
+  //need to add all cards with the img src and maybe something like name/id to compare them later
   this.cards = [
                 "card1",
                 "card2",
@@ -41,30 +42,135 @@ function JungleSpeed() {
                 "card6",
                 "card7",
                 "card8",
-                "card1",
-                "card2",
-                "card3",
-                "card4",
-                "card5",
-                "card6",
-                "card7",
-                "card8"
+                "card9",
+                "card10",
+                "card11",
+                "card12",
+                "card13",
+                "card14",
+                "card15",
+                "card16",
+                "card17",
+                "card18",
+                "card19",
+                "card20",
+                "card21",
+                "card22",
+                "card23",
+                "card24",
+                "card25",
+                "card26",
+                "card27",
+                "card28",
+                "card29",
+                "card30",
+                "card31",
+                "card32",
+                "card33",
+                "card34",
+                "card35",
+                "card36",
+                "card37",
+                "card38",
+                "card39",
+                "card40",
+                "card41",
+                "card42",
+                "card43",
+                "card44",
+                "card45",
+                "card46",
+                "card47",
+                "card48",
+                "card49",
+                "card50",
+                "card51",
+                "card52",
+                "card53",
+                "card54",
+                "card55",
+                "card56",
+                "card57",
+                "card58",
+                "card59",
+                "card60",
+                "card61",
+                "card62",
+                "card63",
+                "card64",
+                "card65",
+                "card66",
+                "card67",
+                "card68",
+                "card69",
+                "card70",
+                "card71",
+                "card72",
+                "card73",
+                "card74",
+                "card75",
+                "card76",
               ];
 
+  this.stackPlayer1 = [];
+  this.stackPlayer2 = [];
+  this.stackPlayer3 = [];
+  this.stackPlayer4 = [];
+
+  this.discardsPlayer1 = [];
+  this.discardsPlayer2 = [];
+  this.discardsPlayer3 = [];
+  this.discardsPlayer4 = [];
+
+
   JungleSpeed.prototype.players = function (player){
-    var cpu = "cpu";
-    if (player !== cpu) {
-      var newPlayer = [prompt("Name of the player:")];
-    }
-    // TODO should be able to choose the lvl of the CPU somehow if it's gonna be a cpu playing
+    // Do i need to put something here?
   };
 
-  JungleSpeed.prototype.splitInStacks = function() {
+  JungleSpeed.prototype.shuffleCards = function() {
     //Gonna split the total card array in 4 equal piles
+    var currentIndex = this.cards.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
+
+    return this.cards;
+
   };
 
-  JungleSpeed.prototype.turnRandomCard = function() {
-    //Gonna create a function that will show one random card everytime the user clicks the show button
+  JungleSpeed.prototype.splitArray = function() {
+
+    var shuffleArray = this.shuffleCards();
+
+    for (var i = 0; i < 19; i++) {
+      this.stackPlayer1.push(shuffleArray[i]);
+    }
+    for (var k = 19; k < 38; k++) {
+      this.stackPlayer2.push(shuffleArray[k]);
+    }
+    for (var j = 38; j < 57; j++) {
+      this.stackPlayer3.push(shuffleArray[j]);
+    }
+    for (var a = 57; a < 76; a++) {
+      this.stackPlayer4.push(shuffleArray[a]);
+    }
+
+  };
+
+  JungleSpeed.prototype.showCard = function() {
+
+    //When the game starts, every 3 seconds countdown, i'll show a card for the user. this card will then got the discard array.
+
   };
 
 }
@@ -75,13 +181,31 @@ function JungleSpeed() {
 // -------------------- INTERACTING WITH THE DOM -----------------------------
 
 //set variables
+var jungleSpeed = new JungleSpeed();
+
+console.log(jungleSpeed.cards);
+jungleSpeed.shuffleCards();
+jungleSpeed.splitArray();
+console.log(jungleSpeed.stackPlayer1);
+console.log(jungleSpeed.stackPlayer2);
+console.log(jungleSpeed.stackPlayer3);
+console.log(jungleSpeed.stackPlayer4);
+
+
+
 var players;
 var playerArr = [];
 var playerKey = ["Q","P","Z","M"];
 var computerLevel = ["dumb","decent","god"];
+var stack1 = [];
+var stack2 = [];
+var stack3 = [];
+var stack4 = [];
+
 
 
 // Check if the user selects 4 real players, if not, it means there will be a computer, so i'll show the computer lvl options
+
 $(document).on('change', "#number-of-players", function(){
 
 var test = $("#number-of-players").val();
@@ -96,6 +220,10 @@ var test = $("#number-of-players").val();
 
 });
 
+// -------------------------------------------------------
+
+
+
 // Get the number of players and ask every human player for his name and assign a key to every human
 
 $( "#all-set" ).on('click', function() {
@@ -106,15 +234,19 @@ $( "#all-set" ).on('click', function() {
 
 });
 
+// --------------------------------------------------------
+
+
+//Click the play button, it will only appear if you set the number of players, after that, launch the board
 
 $( "#play" ).on('click', function() {
 
   $("#first-screen").addClass("hide-container");
   $("#board-screen").addClass("show-container");
 
-
 });
 
+// -------------------------------------------------------
 
 
 
